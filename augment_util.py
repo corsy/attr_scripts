49279f9412b96996855f8f70faf33a8b63711bb8
Luwei Yang <corsy@me.com>
2015-09-18 23:50:47 -0700

    save for update atom

---
+++ augment_util.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Utility of data augmentation
+
+    This script will handling augmentation of training datasets
+    including randomly tranlation and randomly rotate, random zooming.
+
+"""
+
+def add_random_
